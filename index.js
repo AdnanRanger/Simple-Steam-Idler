5ba09c3bc2c1d05f9097cb355bc4527b3593ca66
AdnanRanger <adnan262adnan@gmail.com>
2025-01-29 00:46:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const SteamUser = require('steam-user');
 const user = new SteamUser();
 
 const logOnOptions = {
-	accountName: 'x02dz005traoppy',  //Enter here your account login
-	password: 'MZRPIW4YD1NT116'		 //Enter your password
+	accountName: 'larbom',  //Enter here your account login
+	password: 'Sami007AD'		 //Enter your password
 }
 
 user.logOn(logOnOptions);
@@ -12,5 +12,5 @@ user.logOn(logOnOptions);
 user.on('loggedOn', () => {
 	console.log(logOnOptions.accountName + ' - Successfully logged on');
 	user.setPersona(1);                 //1 - online, 7 - invisible
-	user.gamesPlayed([440,730,570]);    //List app IDs
+	user.gamesPlayed([1091500]);    //List app IDs
 });
